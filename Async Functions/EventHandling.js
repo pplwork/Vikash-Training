e04a8c971362b8ae5d95fa53e4baa549e3419378
VikashPatelDev <vikash@pplwork.com>
2022-05-17 19:34:49 +0530

    eventlistners Done

---
+++ Async Functions/EventHandling.js
@@ -0,0 +1,39 @@
+const outer = document.querySelector(".outer")
+const middle = document.querySelector(".middle")
+const inner = document.querySelector(".inner")
+
+const heading = document.querySelector(".heading")
+const paragraph = document.querySelector(".paragraph")
+
+const keypressEvents = document.querySelector(".keypressEvents")
+
+outer.addEventListener("click", (e) => e.target.style.backgroundColor = "blue")
+middle.addEventListener("click", (e) => e.target.style.backgroundColor = "green")
+inner.addEventListener("click", (e) => e.target.style.backgroundColor = "pink")
+
+
+outer.addEventListener("click", (e) => console.log(e.target.style))
+middle.addEventListener("click", (e) => console.log(e.target))
+inner.addEventListener("click", (e) => console.log(e.target))
+
+middle.addEventListener("click", (e) => e.target.style.backgroundColor = "blue")
+inner.addEventListener("click", (e) => e.target.style.backgroundColor = "blue")
+
+heading.addEventListener("click", (e) => e.target.textContent = "This is Changed Text")
+heading.addEventListener("click", (e) => paragraph.textContent = "This is Changed Text")
+
+
+keypressEvents.addEventListener("click", (e) => console.log(e.target))
+keypressEvents.addEventListener("mousemove",(e)=>{
+    e.target.textContent="yo"
+    setTimeout(()=>{
+        console.log(e.target.textContent="Event after key is pressed")
+    },300)
+})
+keypressEvents.addEventListener('keydown', logKey)
+
+function logKey(e) {
+    console.log(e)
+    keypressEvents.textContent += ` ${e.code}`;
+    inner.textContent = "changed"
+}
